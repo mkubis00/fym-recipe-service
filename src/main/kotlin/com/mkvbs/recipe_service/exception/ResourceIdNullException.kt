38182ce5f2df053ba40af570126410eda48391d3
mkvbs <maciej.k-2000@outlook.com>
2025-10-21 22:23:55 +0200

    Adding new constructor for ResourceIdNullException which takes object without id

--- src/main/kotlin/com/mkvbs/recipe_service/exception/ResourceIdNullException.kt
+++ src/main/kotlin/com/mkvbs/recipe_service/exception/ResourceIdNullException.kt
@@ -4,4 +4,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-class ResourceIdNullException() : FymException("Resource ID can not be null.")
+class ResourceIdNullException : FymException {
+    constructor() : super("Resource ID can not be null.")
+    constructor(objectWithoutId: Any) : super("Resource ID for class ${objectWithoutId::class.simpleName} can not be null, object $objectWithoutId")
+}
