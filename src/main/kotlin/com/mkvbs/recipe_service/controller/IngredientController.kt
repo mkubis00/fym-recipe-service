330d75960bd66196e1c9e217794fdb47cb45c762
mkvbs <maciej.k-2000@outlook.com>
2025-10-27 08:41:54 +0100

    Documentation to REST API was added.

--- src/main/kotlin/com/mkvbs/recipe_service/controller/IngredientController.kt
+++ src/main/kotlin/com/mkvbs/recipe_service/controller/IngredientController.kt
@@ -6,6 +6,9 @@ import com.mkvbs.recipe_service.service.IIngredientService
 import com.mkvbs.recipe_service.utlis.ingredient.toDomain
 import com.mkvbs.recipe_service.utlis.ingredient.toDomainWithId
 import com.mkvbs.recipe_service.utlis.ingredient.toResponseDto
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Size
@@ -24,36 +27,81 @@ import org.springframework.web.bind.annotation.RestController
 
 import java.util.UUID
 
+@Tag(
+    name = "CRUD REST APIs for Ingredients",
+    description = "Rest Api to create, fetch, update and delete ingredients"
+)
 @RestController
 @RequestMapping("/api/ingredient/v1", produces = [MediaType.APPLICATION_JSON_VALUE])
 @Validated
 class IngredientController(
     private val ingredientService: IIngredientService
 ){
+
+    @Operation(
+        summary = "Create Ingredient REST API",
+        description = "REST API to create new ingredient"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "HTTP Status.CREATED"
+    )
     @PostMapping("/addIngredient")
     fun addIngredient(@Valid @RequestBody ingredientDto: IngredientDto): ResponseEntity<IngredientResponseDto> {
         val savedIngredient = ingredientService.addIngredient(ingredientDto.toDomain())
         return ResponseEntity.status(HttpStatus.CREATED).body(savedIngredient.toResponseDto())
     }
 
+    @Operation(
+        summary = "Update Ingredient REST API",
+        description = "REST API to update ingredient"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status.OK"
+    )
     @PutMapping("/updateIngredient")
     fun updateIngredient(@Valid @RequestBody ingredientDto: IngredientDto): ResponseEntity<IngredientResponseDto> {
         val updatedIngredient = ingredientService.updateIngredient(ingredientDto.toDomainWithId())
         return ResponseEntity.status(HttpStatus.OK).body(updatedIngredient.toResponseDto())
     }
 
+    @Operation(
+        summary = "Delete Ingredient REST API",
+        description = "REST API to delete ingredient"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status.OK"
+    )
     @DeleteMapping("/deleteIngredient/{id}")
     fun deleteIngredient(@PathVariable id: UUID): ResponseEntity<IngredientResponseDto> {
         val deletedIngredient = ingredientService.deleteIngredient(id)
         return ResponseEntity.status(HttpStatus.OK).body(deletedIngredient.toResponseDto())
     }
 
+    @Operation(
+        summary = "Fetch Ingredient REST API",
+        description = "REST API to fetch ingredient by ID"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status.OK"
+    )
     @GetMapping("/ingredientById/{id}")
     fun getIngredientById(@PathVariable id: UUID): ResponseEntity<IngredientResponseDto> {
         val ingredient = ingredientService.getIngredientById(id)
         return ResponseEntity.status(HttpStatus.OK).body(ingredient.toResponseDto())
     }
 
+    @Operation(
+        summary = "Get Ingredient REST API",
+        description = "REST API to fetch ingredient by name"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status.OK"
+    )
     @GetMapping("/ingredientByName/{name}")
     fun getIngredientByName(@PathVariable  @NotEmpty(message = "Name can not be null or empty")
                             @Size(min = 3, max = 30, message = "Name can not be less than 3 and not greater than 30 characters") name: String): ResponseEntity<IngredientResponseDto> {
--- src/main/kotlin/com/mkvbs/recipe_service/controller/RecipeController.kt
+++ src/main/kotlin/com/mkvbs/recipe_service/controller/RecipeController.kt
@@ -6,6 +6,9 @@ import com.mkvbs.recipe_service.service.IRecipeService
 import com.mkvbs.recipe_service.utlis.recipe.toDomain
 import com.mkvbs.recipe_service.utlis.recipe.toDomainWithId
 import com.mkvbs.recipe_service.utlis.recipe.toResponseDto
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -21,6 +24,10 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
+@Tag(
+    name = "CRUD REST APIs for Recipes",
+    description = "Rest Api to create, fetch, update and delete recipes"
+)
 @RestController
 @RequestMapping("/api/recipe/v1", produces = [MediaType.APPLICATION_JSON_VALUE])
 @Validated
@@ -28,6 +35,14 @@ class RecipeController(
     private val recipeService: IRecipeService,
 ) {
 
+    @Operation(
+        summary = "Create Recipe REST API",
+        description = "REST API to create new recipe"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "HTTP Status.CREATED"
+    )
     @PostMapping("/addRecipe")
     fun addRecipe(@Valid @RequestBody recipeToCreate: RecipeDto): ResponseEntity<RecipeResponseDto> {
         val recipeDomainToCreate = recipeToCreate.toDomain()
@@ -35,12 +50,28 @@ class RecipeController(
         return ResponseEntity.status(HttpStatus.CREATED).body(savedRecipe.toResponseDto())
     }
 
+    @Operation(
+        summary = "Fetch Recipe REST API",
+        description = "REST API to fetch recipe"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status.OK"
+    )
     @GetMapping("/getRecipeById/{id}")
     fun getRecipeById(@PathVariable id: UUID): ResponseEntity<RecipeResponseDto> {
         val recipe = recipeService.getRecipe(id)
         return ResponseEntity.status(HttpStatus.OK).body(recipe.toResponseDto())
     }
 
+    @Operation(
+        summary = "Update Recipe REST API",
+        description = "REST API to update recipe"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status.OK"
+    )
     @PutMapping("/updateRecipe")
     fun updateRecipe(@Valid @RequestBody recipeDto: RecipeDto): ResponseEntity<RecipeResponseDto> {
         val recipeToUpdate = recipeDto.toDomainWithId()
@@ -48,6 +79,14 @@ class RecipeController(
         return ResponseEntity.status(HttpStatus.OK).body(updatedRecipeDto)
     }
 
+    @Operation(
+        summary = "Delete Recipe REST API",
+        description = "REST API to delete recipe"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status.OK"
+    )
     @DeleteMapping("/deleteRecipe/{id}")
     fun deleteRecipe(@PathVariable id: UUID): ResponseEntity<RecipeResponseDto> {
         val deletedRecipeDto = recipeService.deleteRecipe(id).toResponseDto()
